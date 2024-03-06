861ac4fad311786dd52e62a9bd49daa1db7e1682
daradk1707 <daradk176@gmail.com>
2024-03-06 11:38:30 +0700

    user controller insya allah clear

--- controllers/upload-image.js
+++ controllers/upload-image.js
@@ -1,41 +1,33 @@
 const multer = require(`multer`)
 const path = require(`path`)
 
-/** storage configuration */
 const storage = multer.diskStorage({
-    /** define storage folder */
     destination: (req, file, cb) => {
-        cb(null, `./image`)
+        cb(null, `./image`) //foto disimpen kemana
     },
 
-    /** define filename for upload file */
     filename: (req, file, cb) => {
-        cb(null, `cover-${Date.now()}${path.extname(file.originalname)}`)
+        cb(null, `img-${Date.now()}${path.extname(file.originalname)}`) //nama file nya
     }
 })
 
 const upload = multer({
-    /** storage configuration */
-    storage: storage,
-    /** filter uploaded file */
+    storage: storage, //storage nya dimana
     fileFilter: (req, file, cb) => {
-        /** filter type of file */
-        const acceptedType = [`image/jpg`, `image/jpeg`, `image/png`]
-        if (!acceptedType.includes(file.mimetype)) {
-            cb(null, false) /** refuse upload */
+        const acceptedType = [`image/jpg`, `image/jpeg`, `image/png`] //tipenya apa aja
+        if (!acceptedType.includes(file.mimetype)) { //kalo ga sesuai tipe 
+            cb(null, false) 
             return cb(`Invalid file type (${file.mimetype})`)
 
         }
-
-        /** filter size of file */
-        const fileSize = req.headers[`content-length`]
-        const maxSize = (1 * 1024 * 1024) /** max: 1MB */
-        if(fileSize > maxSize){
-            cb(null, false) /** refuse upload */
+        const fileSize = req.headers[`content-length`] //ukuran file
+        const maxSize = (1 * 2048 * 2048) //max 2 mb
+        if(fileSize > maxSize){ //klo file nya kebesaran
+            cb(null, false) 
             return cb(`File size is too large`)
         }
 
-        cb(null, true) /** accept upload */
+        cb(null, true) 
     }
 })
 module.exports = upload
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,32 +25,29 @@ exports.RegisterCustomer = (request, response) => { //buat user baru yang belum 
             password: md5(request.body.password),
             role: "user",
         };
-        
+
         let user = await userModel.findAll({
             where: { //cari, udah ada apa blm datanya
                 [Op.or]: [{ nama: newUser.nama }, { email: newUser.email }],
             },
         });
-        
+
         if (
             newUser.nama === "" ||
             newUser.email === "" ||
             newUser.password === ""
-            ) {
+        ) {
             const oldFotoUser = newUser.foto;
             const patchFoto = path.join(__dirname, `../foto`, oldFotoUser); //fotonya disimpen di direktori yang itu
-            if (fs.existsSync(patchFoto)) {
+            if (fs.existsSync(patchFoto)) { //ini kalo error soalnya ga semuanya keisi. footnya ga bakal ke upload klo masiha da yang blm diisi
                 fs.unlink(patchFoto, (error) => console.log(error));
             }
-
             return response.status(400).json({
                 success: false,
                 message: "Harus diisi semua",
             });
-        } else {
-            //nama dan email tidak boleh sama
+        } else { //dia udah punya akun dan login nya pake data yg sama
             if (user.length > 0) {
-                //karena gagal hapus foto yang masuk
                 const oldFotoUser = newUser.foto;
                 const patchFoto = path.join(__dirname, `../foto`, oldFotoUser);
                 if (fs.existsSync(patchFoto)) {
@@ -60,7 +57,7 @@ exports.RegisterCustomer = (request, response) => { //buat user baru yang belum 
                     success: false,
                     message: "Cari nama atau email lain",
                 });
-            } else {
+            } else { //ini dia murni bikin akun baru, prosesnya kayak add user biasa
                 console.log(newUser);
                 userModel
                     .create(newUser)
@@ -68,7 +65,7 @@ exports.RegisterCustomer = (request, response) => { //buat user baru yang belum 
                         return response.json({
                             success: true,
                             data: result,
-                            message: `New User has been inserted`,
+                            message: `New User has been added`,
                         });
                     })
                     .catch((error) => {
@@ -77,8 +74,8 @@ exports.RegisterCustomer = (request, response) => { //buat user baru yang belum 
                             message: error.message,
                         });
                     });
-                }
             }
+        }
     });
 }
 
@@ -122,7 +119,7 @@ exports.Login = async (request, response) => {
 }
 
 //ini sebenernya buat ngecek doang si user uda login apa belum, ntar klo udah ada frontend nya baru keliatan fungsinya
-exports.LoginRegister = async (request, response) => { 
+exports.LoginRegister = async (request, response) => {
     const email = request.body.email //masukin username
     let user = await userModel.findAll({
         where: { role: "user", email: email } //cari data yang sesuai
@@ -165,101 +162,288 @@ exports.LoginRegister = async (request, response) => {
     }
 }
 
-
-
-exports.getAllUser = async (request, response) => {
-    let users = await userModel.findAll()
-    return response.json({
-        success: true,
-        data: users,
-        message: `All User have been loaded `
-    })
-}
-
-exports.findUser = async (request,response) => {
-    let keyword = request.body.keyword
-    let users = await userModel.findAll({ 
-        where: {
-            [Op.or]: [
-                { UserID: { [Op.substring]: keyword } },
-                { nama: { [Op.substring]: keyword } },
-                { email: { [Op.substring]: keyword } },
-                { password: { [Op.substring]: keyword } }
-            ]
+exports.addUser = (request, response) => { //hampir sama kayak register, tp klo ini yang bisa akses si admin
+    upload(request, response, async error => {
+        if (error) {
+            return response.json({ message: error })
         }
-    })
-        return response.json({
-            success: true,
-            data:users,
-            message:`All users have been loaded`
-        })
-}
+        if (!request.file) {
+            return response.json({ message: `Nothing to upload` })
+        }
 
-exports.addUser = (request,response) => {
-    let newUser = {
-        nama: request.body.nama,
-        email: request.body.email,
-        password: md5(request.body.password)
-    }
-    if (request.body.password) {
-        newUser.password = md5(request.body.password);
-    }
+        let newUser = {
+            nama: request.body.nama,
+            foto: request.file.filename,
+            email: request.body.email,
+            password: md5(request.body.password),
+            role: request.body.role
+        }
 
-    userModel.create(newUser).then(result => {
-        return response.json({
-            success: true,
-            data: result,
-            message: `New User has been inserted`
-        })
-    })
-    .catch(error => {
-        return response.json({
-            success: false,
-            message: error.message
-        })
+        let user = await userModel.findAll({
+            where: {
+                [Op.or]: [{ nama_user: newUser.nama_user }, { username: newUser.username }], //ngecek ada apa nggak usernya
+            },
+        });
+
+        if (
+            newUser.nama_user === "" ||
+            newUser.username === "" ||
+            newUser.password === "" ||
+            newUser.role === ""
+        ) {
+            //karena gagal, hapus foto yang masuk
+            const oldFotoUser = newUser.foto;
+            const patchFoto = path.join(__dirname, `../foto`, oldFotoUser);
+            if (fs.existsSync(patchFoto)) {
+                fs.unlink(patchFoto, (error) => console.log(error));
+            }
+
+            return response.status(400).json({
+                success: false,
+                message: "Harus diisi semua",
+            });
+        } else {
+            //nama dan email tidak boleh sama
+            if (user.length > 0) {
+                //karena gagal hapus foto yang masuk
+                const oldFotoUser = newUser.foto;
+                const patchFoto = path.join(__dirname, `../foto`, oldFotoUser);
+                if (fs.existsSync(patchFoto)) {
+                    fs.unlink(patchFoto, (error) => console.log(error));
+                }
+                return response.status(400).json({
+                    success: false,
+                    message: "Cari nama atau email lain",
+                });
+            } else {
+                console.log(newUser);
+                userModel
+                    .create(newUser)
+                    .then((result) => {
+                        return response.json({
+                            success: true,
+                            data: result,
+                            message: `New User has been inserted`,
+                        });
+                    })
+                    .catch((error) => {
+                        return response.status(400).json({
+                            success: false,
+                            message: error.message,
+                        });
+                    });
+            }
+        }
     })
 }
 
 exports.updateUser = (request, response) => {
-    let dataUser = {
-        UserID: request.body.UserID,
-        nama: request.body.nama,
-        email: request.body.email,
-        password: request.body.password
-    }
-    if (request.body.password) {
-        dataUser.password = md5(request.body.password);
-    }
+    upload(request, response, async (error) => {
+        if (error) {
+            return response.status(400).json({ message: error });
+        }
 
-    let UserID = request.params.UserID
-    userModel.update(dataUser, { where: { UserID : UserID } })
-        .then(result => {
-            return response.json({
-                success: true,
-                message: `Data user has been updated`
-            })
-        })
-        .catch(error => {
-            return response.json({
+        let UserID = request.params.id; //user mana yang mau di update
+
+        let getId = await userModel.findAll({ //dicari usernya
+            where: {
+                [Op.and]: [{ id: UserID }],
+            },
+        });
+
+        if (getId.length === 0) { //klo ga nemu
+            return response.status(400).json({
                 success: false,
-                message: error.message
+                message: "User dengan id tersebut tidak ada",
+            });
+        }
+
+        let dataUser = { //data terbaru yang udah di update
+            nama: request.body.nama,
+            email: request.body.email,
+            password: md5(request.body.password),
+            role: request.body.role,
+            foto: getId.foto, //sementara fotonya tetep
+        };
+
+        if (request.file) { //klo ternyata ganti foto
+            const selectedUser = await userModel.findOne({ //dicari yag mau ganti foto
+                where: { id: UserID },
+            });
+
+            const oldFotoUser = selectedUser.foto;
+            const patchFoto = path.join(__dirname, `../foto`, oldFotoUser);
+
+            if (fs.existsSync(patchFoto)) {
+                fs.unlink(patchFoto, (error) => console.log(error));
+            }
+
+            dataUser.foto = request.file.filename; //fotonya udah ke update
+        }
+
+        if ( //kalo ada yang kosong
+            dataUser.nama_user === "" ||
+            dataUser.username === "" ||
+            dataUser.password === "" ||
+            dataUser.role === ""
+        ) {
+            return response.status(400).json({
+                success: false,
+                message:
+                    "Harus diisi semua.Kalau tidak ingin merubah, isi dengan value sebelumnya",
+            });
+        }
+
+        let user = await userModel.findAll({
+            where: {
+                [Op.and]: [
+                    { id: { [Op.ne]: UserID } },
+                    {
+                        [Op.or]: [
+                            { nama_user: dataUser.nama_user }, //cek, nama sama emailnya udah dipake orang lain apa belum
+                            { username: dataUser.username },
+                        ],
+                    },
+                ],
+            },
+        });
+
+        if (user.length > 0) { //kalo ternyata udah dipake
+            return response.status(400).json({
+                success: false,
+                message: "Cari nama atau email lain",
+            });
+        }
+
+        userModel
+            .update(dataUser, { where: { id: idUser } })
+            .then((result) => {
+                return response.json({
+                    success: true,
+                    message: `Data user has been updated`,
+                });
             })
-        })
+            .catch((error) => {
+                return response.status(400).json({
+                    success: false,
+                    message: error.message,
+                });
+            });
+    });
 }
 
-exports.deleteUser = (request, response) => {
-    let UserID = request.params.UserID
-    userModel.destroy({ where: { UserID: UserID } })
-        .then(result => {
+exports.getAllUser = async (request, response) => {
+    let user = await userModel.findAll()
+    if (user.length === 0) {
+        return response.status(400).json({
+            success: false,
+            message: "no user to show",
+        });
+    }
+    return response.json({
+        success: true,
+        data: user,
+        message: `All user have been loaded`
+    })
+}
+
+exports.findUser = async (request, response) => {
+    let id = request.params.id;
+    if (!id) { //kalo ga masukin ID
+        return response.status.json({
+            success: false,
+            message: "masukkan id user di url",
+        });
+    } else { //dicari user yang sesuai id nya
+        let user = await userModel.findOne({
+            where: {
+                [Op.and]: [{ id: id }],
+            },
+        });
+
+        if (!user) { //kalo usernya ga ada
+            return response.status(400).json({
+                success: false,
+                message: "no user to show",
+            });
+        } else { //kalo ada
             return response.json({
                 success: true,
-                message: `Data user has been deleted`
-            })
-        })
-        .catch(error => {
+                data: user,
+                message: `User have been loaded`,
+            });
+        }
+    }
+}
+
+exports.findAllCustomer = async (request, response) => {
+    let user = await userModel.findAll({ where: { role: "customer" } }); //dicari yang role nya customer/user
+    if (user.length === 0) { //klo ga ada
+        return response.status(400).json({
+            success: false,
+            message: "no user to show",
+        });
+    } else { //kalo ada
+        return response.json({
+            success: true,
+            data: user,
+            message: `All User have been loaded`,
+        });
+    }
+}
+
+exports.findAllAdmin = async (request, response) => {
+    let admin = await userModel.findAll({ where: { role: "admin" } }); //dicari yang role nya admin
+    if (user.length === 0) { //klo ga ada
+        return response.status(400).json({
+            success: false,
+            message: "no admin to show",
+        });
+    } else { //kalo ada
+        return response.json({
+            success: true,
+            data: user,
+            message: `All admin have been loaded`,
+        });
+    }
+}
+
+exports.deleteUser = async (request, response) => {
+    let UserID = request.params.id; //cari user berdasarkan ID
+    let getId = await userModel.findAll({
+        where: { //dicari 
+            [Op.and]: [{ id: UserID }],
+        },
+    });
+
+    if (getId.length === 0) { //kalo ga ada yang sesuai
+        return response.status(400).json({
+            success: false,
+            message: "User dengan id tersebut tidak ada",
+        });
+    }
+
+    const user = await userModel.findOne({ where: { id: UserID } }); //data sesuai id nya
+    const oldFotoUser = user.foto; //foto lama
+    const patchFoto = path.join(__dirname, `../foto`, oldFotoUser); //dicari direktorinya dimana
+
+    if (fs.existsSync(patchFoto)) {
+        fs.unlink(patchFoto, (error) => console.log(error));
+    }
+
+    userModel
+        .destroy({ where: { id: UserID } })
+
+        .then((result) => {
             return response.json({
-                success: false,
-                message: error.message
-            })
+                success: true,
+                message: `data user has ben delete where id :` + idUser,
+            });
         })
+        .catch((error) => {
+            return response.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        });
 }
